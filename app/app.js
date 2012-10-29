dad3150a28aaaa44324e399df5cafa73b545d427
Jr <jr@ailleurs.me>
2012-10-29 14:05:10 -0700

    better mongoDB support and code cleaning

--- app/app.js
+++ app/app.js
@@ -5,9 +5,11 @@ var app = express();
 /* mongodb setup */
 var mongo = require('mongodb');
 
-var host = (process.env['DOTCLOUD_DB_MONGODB_HOST'])? process.env['DOTCLOUD_DB_MONGODB_HOST'] : 'localhost';
-var port = (process.env['DOTCLOUD_DB_MONGODB_PORT'])? process.env['DOTCLOUD_DB_MONGODB_PORT'] : 27017;
+var host = process.env['DOTCLOUD_DB_MONGODB_HOST'] || 'localhost';
+var port = process.env['DOTCLOUD_DB_MONGODB_PORT'] ||  27017;
 port = parseInt(port);
+var user = process.env['DOTCLOUD_DB_MONGODB_LOGIN'] || undefined;
+var pass = process.env['DOTCLOUD_DB_MONGODB_PASSWORD'] || undefined;
 
 var mongoServer = new mongo.Server(host, port, {});
 var db = new mongo.Db("test", mongoServer, {auto_reconnect:true});
@@ -17,10 +19,24 @@ app.get("/", function(req, res){
     html += 'Hello World, from Express!';
     html += '<script type="text/javascript" src="https://helloapp.dotcloud.com/inject.min.js"></script>';
 
-    res.send(html);
+    db.collection("test", function(err, collection){
+        if(err) console.log(err);
+        collection.find(function(err, cursor){
+            if(err) console.log(err);
+            res.send(html);
+        });
+    })
 });
 
-db.open(function(err, db){
+db.open(function(err){
     if(err) console.log(err);
-    app.listen(8080);
+
+    if(user && pass) {
+        db.authenticate(user, pass, function(err) {
+            app.listen(8080);
+        });
+    }
+    else {
+        app.listen(8080);
+    }
 });
