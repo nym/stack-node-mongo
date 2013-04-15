1a69b263bd2dc1bf9ad0c74ff56fc6ca8f468787
Andy Rothfusz <github@metaliveblog.com>
2013-04-15 15:19:37 -0700

    Use the admin database which has user 'root'.
    test database doesn't have any users by default, so can't auth to it.

--- app/app.js
+++ app/app.js
@@ -12,14 +12,14 @@ var user = process.env['DOTCLOUD_DB_MONGODB_LOGIN'] || undefined;
 var pass = process.env['DOTCLOUD_DB_MONGODB_PASSWORD'] || undefined;
 
 var mongoServer = new mongo.Server(host, port, {});
-var db = new mongo.Db("test", mongoServer, {auto_reconnect:true,w:'majority'});
+var db = new mongo.Db("admin", mongoServer, {auto_reconnect:true,w:'majority'});
 
 app.get("/", function(req, res){
     var html = '<div id="content" data-stack="node" data-appname="' + process.env['DOTCLOUD_PROJECT'] + '">';
     html += 'Hello World, from Express!';
     html += '<script type="text/javascript" src="https://helloapp.dotcloud.com/inject.min.js"></script>';
 
-    db.collection("test", function(err, collection){
+    db.collection("admin", function(err, collection){
         if(err) console.log(err);
         collection.find(function(err, cursor){
             if(err) console.log(err);
@@ -33,6 +33,7 @@ db.open(function(err){
 
     if(user && pass) {
         db.authenticate(user, pass, function(err) {
+            console.log('Successfully authenticated to database.')
             app.listen(8080);
         });
     }
